301fbe1c94fcd6f03ede0e04e58ff46e0374f3b8
flaviuspopan <flaviuspopan@gmail.com>
2019-11-18 09:41:28 -0700

    Sync script added

---
+++ sync_fork.sh
@@ -0,0 +1,5 @@
+# Config [DONE] - https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/configuring-a-remote-for-a-fork
+# Sync - https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/syncing-a-fork
+git fetch upstream
+git checkout master
+git merge upstream/master
